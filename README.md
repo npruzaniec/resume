ab33e0fcff4b92b6375b70123138e9361508f8ac
npruzaniec <npruzaniec@newlifechurch.tv>
2019-08-27 15:38:36 -0700

    initial content and sass setup

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # resume
 This is the resume of Nathan Pruzaniec.
+
+
+gem install haml
+gem install sass
+
+
+
+haml index.haml index.html
+sass --watch styles.scss:styles.css
---
+++ data.rb
@@ -0,0 +1,21 @@
+- books = ['book 1', 'book 2', 'book 3']
+
+
+/- jobs = [{:title => "Director",
+  :company => "/docs",
+  :dates => "/about",
+  :pts => ["work hard",
+    "play hard",
+    "work hard"]},
+  {:title => "Standup Guy",
+  :company => "/docs",
+  :dates => "/about",
+  :pts => ["work hard",
+    "play hard",
+    "work hard"]},
+  {:title => "Fiddler on the Roof",
+  :company => "/docs",
+  :dates => "/about",
+  :pts => ["work hard",
+    "play hard",
+    "work hard"]}]
